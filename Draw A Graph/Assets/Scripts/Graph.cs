95c0adec30c6f2880c9b3968fdc946077d9bfd6f
Ciaran <joshuaredirect@gmail.com>
2026-03-27 04:48:46 -0500

    added graph grids

--- Draw A Graph/Assets/Scripts/Graph.cs
+++ Draw A Graph/Assets/Scripts/Graph.cs
@@ -5,7 +5,7 @@ public class Graph : MonoBehaviour {
     Transform pointPreFab;
 
     [SerializeField, Range(10, 100)]
-    int resolution = 10;
+    int resolution = 100;
 
     [SerializeField]
     FunctionLibrary.FunctionName function;
@@ -17,11 +17,18 @@ public class Graph : MonoBehaviour {
         float step = 2f / resolution;
 		var position = Vector3.zero;
 		var scale = Vector3.one * step;
-        points = new Transform[resolution];
-        for (int i = 0; i < resolution; ++i) {
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPreFab);
             point.SetParent(transform, false);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
         }
@@ -35,7 +42,7 @@ public class Graph : MonoBehaviour {
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
